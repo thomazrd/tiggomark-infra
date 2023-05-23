82a77471650076ea5597232a9b8605e37cb7bb72
tiggomark <thomazrdamasceno@gmail.com>
2023-05-23 11:31:10 -0300

    Create saas connector volume

---
+++ configure-vm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Atualizar repositórios
+sudo apt update
+
+# Instalar pacotes pré-requisitos
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Adicionar chave GPG oficial do Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Adicionar repositório estável do Docker
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Atualizar repositórios novamente
+sudo apt update
+
+# Instalar Docker Engine
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Adicionar usuário atual ao grupo "docker" para executar comandos Docker sem sudo
+sudo usermod -aG docker $USER
+
+# Habilitar inicialização automática do Docker no boot
+sudo systemctl enable docker
+
+# Instalar o comando docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Verificar a versão do Docker instalada
+docker --version
+
+# Verificar a versão do docker-compose instalado
+docker-compose --version
+
+# Mostrar informações do Docker
+docker info
+
+# Informar ao usuário que a instalação foi concluída
+echo "Docker foi instalado e configurado com sucesso!"
+
+# Iniciar cluster Swarm
+docker swarm init
